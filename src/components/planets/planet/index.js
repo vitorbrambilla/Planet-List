0e8c4d6c7146939c99333e39f1b1d04ddb62963b
vitorbrambilla <vitorbrambilla10@gmail.com>
2021-12-22 12:04:47 -0300

    conditional rendering and multiple rendering

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -3,12 +3,25 @@ import GrayImg from '../../shared/gray_img';
 import DescriptionWithLink from '../../shared/description_with_link';
 
 const Planet = (props) => {
-    return (   
+    let title;
+    if (props.title_with_underline)
+        title = <h4><u>{props.name}</u></h4>
+    else
+        title = <h4>{props.name}</h4>
+
+    return (
         <div>
-            <h4>{props.name}</h4>
-            <DescriptionWithLink description={props.description} link={props.link}/>
-            <GrayImg img_url={props.img_url}/>
-            <hr/>
+            {title}
+            <DescriptionWithLink description={props.description} link={props.link} />
+            <GrayImg img_url={props.img_url} gray={props.gray} />
+
+            <h4>Satélites</h4>
+            <ul>
+                {['a', 'b', 'c', 'd'].map((n) =>
+                    <li>Satélite {n}</li>
+                )}
+            </ul>
+            <hr />
         </div>
     );
 }
